5b7f287d3d7e5fbbfec5527136bf05eb17c3fe6a
Radostin Dragiev <56969624+RadostinDragiev@users.noreply.github.com>
2023-05-30 23:06:59 +0300

    Minor changes

--- src/main/java/com/example/mobilelele/web/AuthController.java
+++ src/main/java/com/example/mobilelele/web/AuthController.java
@@ -6,6 +6,7 @@ import com.example.mobilelele.web.dtos.AuthUserDto;
 import com.example.mobilelele.web.dtos.RegisterUserDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/users")
@@ -25,12 +27,16 @@ public class AuthController {
     }
 
     @GetMapping("/login")
-    public String getLogin(HttpSession httpSession) {
+    public String getLogin(HttpSession httpSession, @Valid @ModelAttribute("authUserDto") AuthUserDto authUserDto, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
         return "auth-login";
     }
 
     @PostMapping("/login")
-    public String login(AuthUserDto authUserDto, HttpSession httpSession) {
+    public String login(@Valid @ModelAttribute("authUserDto") AuthUserDto authUserDto, BindingResult bindingResult, HttpSession httpSession, RedirectAttributes redirectAttributes) {
+        if (bindingResult.hasErrors()) {
+            redirectAttributes.addFlashAttribute("authUserDto", authUserDto);
+            return "redirect:/users/login";
+        }
         User user = this.userService.login(authUserDto);
         if (user != null) {
             httpSession.setAttribute("userId", user.getUuid());
@@ -38,6 +44,7 @@ public class AuthController {
             return "redirect:/";
         } else {
             httpSession.setAttribute("errMsg", "Invalid username or password.");
+            redirectAttributes.addFlashAttribute("authUserDto", authUserDto);
             return "redirect:/users/login";
         }
     }
--- src/main/java/com/example/mobilelele/web/dtos/AuthUserDto.java
+++ src/main/java/com/example/mobilelele/web/dtos/AuthUserDto.java
@@ -4,12 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.Min;
 
 @Setter
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class AuthUserDto {
+    @Length(min = 2, message = "Username is too short")
     private String username;
+    @Length(min = 2, message = "Password is too short")
     private String password;
 }
